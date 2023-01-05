aab4237b3e9185664ff0cd8c18068d90894883fa
dhruvdeveloper <121082016+dhruvdeveloper@users.noreply.github.com>
2023-01-06 00:29:46 +0530

    Update antiflood.py

--- FallenRobot/modules/antiflood.py
+++ FallenRobot/modules/antiflood.py
@@ -418,7 +418,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "Aɴᴛɪ-Fʟᴏᴏᴅ"
+__mod_name__ = "𝐴𝑛𝑡𝑖-𝐹𝑙𝑜𝑜𝑑"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood
